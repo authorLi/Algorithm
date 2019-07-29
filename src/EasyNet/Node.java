5904dc9ab6aae425f0ae21062ea36bbbd9e35587
authorLi <www.524894052@qq.com>
2019-07-29 18:00:19 +0800

    单链表节点Node及相关操作和验证

--- src/EasyNet/Node.java
+++ src/EasyNet/Node.java
@@ -5,4 +5,51 @@ package EasyNet;
  * @createTime 2019/7/29 10:58
  */
 public class Node {
+
+    private int val;
+
+    private Node next;
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    //尾插法:每次都插入到尾部
+    public void append(Node node) {
+        Node cur = this;
+        while (cur.next != null) {
+            cur = cur.next;
+        }
+        cur.next = node;
+    }
+
+    public Node next() {
+        return this.next;
+    }
+
+    public int getVal() {
+        return this.val;
+    }
+
+    public boolean isLast() {
+        return this.next == null ? true : false;
+    }
+
+    public void removeNext(){
+        this.next = this.next.next;
+    }
+
+    public void show(){
+        Node cur = this;
+        while (cur != null){
+            System.out.println(cur.val);
+            cur = cur.next;
+        }
+    }
+
+    //插入一个节点作为当前节点的下一个节点
+    public void after(Node node){
+        node.next = this.next;
+        this.next = node;
+    }
 }
---
+++ src/EasyNet/TestNode.java
@@ -0,0 +1,28 @@
+package EasyNet;
+
+/**
+ * @author mycclee
+ * @createTime 2019/7/29 11:08
+ */
+public class TestNode {
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        Node node6 = new Node(6);
+//        node1.append(node2);
+//        node2.append(node3);
+//        node3.append(node4);
+//        node4.append(node5);
+//        node5.append(node6);
+        node1.append(node2);
+        node1.append(node3);
+        node1.append(node4);
+        node1.append(node5);
+        node1.append(node6);
+        node1.show();
+    }
+}
